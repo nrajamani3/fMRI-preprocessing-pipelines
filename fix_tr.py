c67750c9b3c0da3d848610ca4b2742f5e1fd0fcd
nrajamani3 <nrajamani3@gatech.edu>
2019-09-24 18:15:04 +0530

    remove hard coded path

--- fix_tr.py
+++ fix_tr.py
@@ -3,7 +3,7 @@ import sys
 import subprocess
 import commands
 
-file_with_bad_tr = '/Users/nanditharajamani/Desktop/IIT_delhi_stuff/Assignment_IIT_delhi/fsl_preprocessed/preproc/fsl_preprocess/smooth_img/sub-MSC01_ses-func01_task-motor_run-01_bold_brain_mcf_st_smooth.nii.gz'
+file_with_bad_tr = 'path/to/file/with/bad/tr'
 #output only the pixel dmensions, the pixdim4 is the TR.
 get_header = commands.getstatusoutput('fslinfo {0} | grep "pixdim"'.format(file_with_bad_tr))
 #this will split the str with all the pixdim values
